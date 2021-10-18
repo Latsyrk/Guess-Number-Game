afe7d12d7834d54fdfeb267ca77dafc81bccc43e
diertapasific <diertapasific@gmail.com>
2021-10-18 13:29:57 +0700

    Enter

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+for(int i=0; i<10000;i++){
+	// 	printf("\nDo you want to play again?(enter number)\n1 Yes\n2 No\n"); // Give the user choice whether they want to play again or not
+	// 	scanf("%d", &pilihan);
+	// 	// choose(pilihan); // use choose function(line 90) 
+	// }
